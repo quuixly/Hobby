3f75a80963528dc96e2a39658c88fc1473693082
quuixly <quuixly@gmail.com>
2026-08-03 20:47:58 +0200

    [PyTorch-ClassArch] feat: Add basic train loop and neural network

---
+++ PyTorch-ClassificationArchitecture/HandwrittenDigitsClassification/main.py
@@ -0,0 +1,65 @@
+import tqdm
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from datasets import load_dataset
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+dataset = load_dataset("ylecun/mnist")
+dataset.set_format(type="torch", columns=["image", "label"], device=device)
+
+train_loader = DataLoader(dataset["train"], batch_size=64, shuffle=True)
+test_loader = DataLoader(dataset["test"], batch_size=64, shuffle=False)
+
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear1 = nn.Linear(28 * 28, 512)
+        self.linear2 = nn.Linear(512, 10)
+
+    def forward(self, x):
+        x = x.view(-1, 28 * 28)
+        x = F.relu(self.linear1(x))
+        x = self.linear2(x)
+
+        return x
+
+
+class Trainer:
+    def __init__(self, model, optimizer, criteria):
+        self.model = model
+        self.optimizer = optimizer
+        self.criteria = criteria
+
+    def train(self, dataloader, num_epoch):
+        self.model.train()
+        
+        for epoch in range(num_epoch):
+            pbar = tqdm.tqdm(dataloader, desc = f"Epoch {epoch}/{num_epoch}")
+            total_loss = 0
+            for batch in pbar:
+                inputs = batch["image"].float()
+                outputs = batch["label"]
+
+                self.optimizer.zero_grad()
+                predictions = self.model(inputs)
+                loss = self.criteria(predictions, outputs)
+
+                total_loss += loss
+
+                loss.backward()
+                self.optimizer.step()
+
+            pbar.set_postfix(loss=f"total_loss")
+        
+
+model = NeuralNetwork().to(device)
+optimier = optim.AdamW(model.parameters())
+criteria = nn.CrossEntropyLoss()
+trainer = Trainer(model, optimier, criteria)
+trainer.train(train_loader, 5)
